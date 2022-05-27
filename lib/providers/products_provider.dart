fc4fc8cefb3d349409c586d5b8cf2aac5eba3e61
loran <laurent.muller@wizbii.com>
2022-05-27 20:22:48 +0200

    FEAT--application-state: Added provider for  each product

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/mocks/products.dart';
 import 'package:shop_app/providers/product_provider.dart';
 
-import '../models/product.dart';
-
 class ProductsProvider with ChangeNotifier {
   final List<ProductProvider> _items = Products;
 
--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/product.dart';
 
 import '../mocks/products.dart';
 import '../providers/products_provider.dart';
